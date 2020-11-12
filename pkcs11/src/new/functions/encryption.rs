fa8dfa988ac64899ee069e23aea2ce2589ec5775
Hugues de Valon <hugues.devalon@arm.com>
2020-11-12 10:48:27 +0000

    Add basic example
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- pkcs11/src/new/functions/encryption.rs
+++ pkcs11/src/new/functions/encryption.rs
@@ -1,11 +1,17 @@
 use crate::new::types::function::Result;
-use crate::new::types::session::Session;
-use crate::new::types::object::Object;
-use crate::new::Pkcs11;
 use crate::new::types::mechanism::Mechanism;
+use crate::new::types::object::Object;
+use crate::new::types::session::Session;
+use crate::new::Pkcs11;
 
 impl Pkcs11 {
-    pub fn encrypt(&self, session: &Session, mechanism: Mechanism, key: &Object, data: Vec<u8>) -> Result<Vec<u8>> {
+    pub fn encrypt(
+        &self,
+        _session: &Session,
+        _mechanism: Mechanism,
+        _key: &Object,
+        _data: &[u8],
+    ) -> Result<Vec<u8>> {
         Ok(Vec::new())
     }
 }
--- pkcs11/src/new/mod.rs
+++ pkcs11/src/new/mod.rs
@@ -7,7 +7,8 @@ use std::mem;
 use std::path::Path;
 
 pub struct Pkcs11 {
-    pkcs11_lib: pkcs11_sys::Pkcs11,
+    //this field is not needed once we have the function list
+    //pkcs11_lib: pkcs11_sys::Pkcs11,
     function_list: *mut pkcs11_sys::_CK_FUNCTION_LIST,
 }
 
@@ -35,7 +36,6 @@ impl Pkcs11 {
             let list_ptr = *list.as_ptr();
 
             Ok(Pkcs11 {
-                pkcs11_lib,
                 function_list: list_ptr,
             })
         }
@@ -62,3 +62,85 @@ impl From<libloading::Error> for Error {
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use crate::new::types::mechanism::Mechanism;
+    use crate::new::types::object::Attribute;
+    use crate::new::types::session::UserType;
+    use crate::new::types::Flags;
+    use crate::new::Pkcs11;
+
+    #[test]
+    fn the_one_test() {
+        let pkcs11 = Pkcs11::new("/usr/local/lib/softhsm/libsofthsm2.so").unwrap();
+
+        // find a slot, get the first one
+        let slot = pkcs11.get_slots_with_token().unwrap().remove(0);
+
+        // set flags
+        let mut flags = Flags::new();
+        flags.set_rw_session(true).set_serial_session(true);
+
+        // open a session
+        let session = pkcs11.open_session(&slot, flags).unwrap();
+
+        let pin = String::from("123456");
+
+        // log in the session
+        pkcs11.login(&session, UserType::User, pin).unwrap();
+
+        // get mechanism
+        let mechanism = Mechanism::RsaPkcsKeyPairGen;
+
+        let mut attr_true: u8 = 1;
+        let mut attr_true1: u8 = 1;
+        let mut attr_false: u8 = 0;
+        let mut public_exponent: Vec<u8> = vec![0x00, 0x01, 0x00];
+        let mut modulus_bits: u64 = 1024;
+
+        // pub key template
+        let mut pub_key_template = vec![
+            Attribute::Token(&mut attr_true),
+            Attribute::Private(&mut attr_false),
+            Attribute::PublicExponent(&mut public_exponent),
+            Attribute::ModulusBits(&mut modulus_bits),
+        ];
+
+        // priv key template
+        let mut priv_key_template = vec![Attribute::Token(&mut attr_true1)];
+
+        // generate a key pair
+        let (public, private) = pkcs11
+            .generate_key_pair(
+                &session,
+                mechanism,
+                &mut pub_key_template,
+                &mut priv_key_template,
+            )
+            .unwrap();
+
+        // data to sign
+        let data = [0xFF, 0x55, 0xDD];
+
+        // sign something with it
+        let signature = pkcs11
+            .sign(&session, Mechanism::RsaPkcs, &private, &data)
+            .unwrap();
+
+        // verify the signature
+        pkcs11
+            .verify(&session, Mechanism::RsaPkcs, &public, &data, &signature)
+            .unwrap();
+
+        // delete keys
+        pkcs11.destroy_object(&session, public).unwrap();
+        pkcs11.destroy_object(&session, private).unwrap();
+
+        // log out
+        pkcs11.logout(&session).unwrap();
+
+        // close session
+        pkcs11.close_session(session).unwrap();
+    }
+}
