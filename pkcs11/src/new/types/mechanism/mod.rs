9609aed9cd19a0603b28720c841c286b50b5aaf8
Hugues de Valon <hugues.devalon@arm.com>
2020-11-23 12:42:50 +0000

    Complete with some more mechanisms
    Add RSA OAEP and PSS.
    
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- pkcs11/src/new/types/mechanism/mod.rs
+++ pkcs11/src/new/types/mechanism/mod.rs
@@ -1,9 +1,10 @@
-mod rsa;
+pub mod rsa;
 
 use crate::new::Error;
 use log::error;
 use pkcs11_sys::*;
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
+use std::ffi::c_void;
 use std::ops::Deref;
 use std::ptr::null_mut;
 
@@ -16,10 +17,23 @@ pub struct MechanismType {
 }
 
 impl MechanismType {
-    const RSA_PKCS_KEY_PAIR_GEN: MechanismType = MechanismType {
+    // RSA
+    pub const RSA_PKCS_KEY_PAIR_GEN: MechanismType = MechanismType {
         val: CKM_RSA_PKCS_KEY_PAIR_GEN,
     };
-    const RSA_PKCS: MechanismType = MechanismType { val: CKM_RSA_PKCS };
+    pub const RSA_PKCS: MechanismType = MechanismType { val: CKM_RSA_PKCS };
+    pub const RSA_PKCS_PSS: MechanismType = MechanismType {
+        val: CKM_RSA_PKCS_PSS,
+    };
+    pub const RSA_PKCS_OAEP: MechanismType = MechanismType {
+        val: CKM_RSA_PKCS_OAEP,
+    };
+
+    // SHA-n
+    pub const SHA1: MechanismType = MechanismType { val: CKM_SHA_1 };
+    pub const SHA256: MechanismType = MechanismType { val: CKM_SHA256 };
+    pub const SHA384: MechanismType = MechanismType { val: CKM_SHA384 };
+    pub const SHA512: MechanismType = MechanismType { val: CKM_SHA512 };
 }
 
 impl Deref for MechanismType {
@@ -43,6 +57,12 @@ impl TryFrom<CK_MECHANISM_TYPE> for MechanismType {
         match mechanism_type {
             CKM_RSA_PKCS_KEY_PAIR_GEN => Ok(MechanismType::RSA_PKCS_KEY_PAIR_GEN),
             CKM_RSA_PKCS => Ok(MechanismType::RSA_PKCS),
+            CKM_RSA_PKCS_PSS => Ok(MechanismType::RSA_PKCS_PSS),
+            CKM_RSA_PKCS_OAEP => Ok(MechanismType::RSA_PKCS_OAEP),
+            CKM_SHA_1 => Ok(MechanismType::SHA1),
+            CKM_SHA256 => Ok(MechanismType::SHA256),
+            CKM_SHA384 => Ok(MechanismType::SHA384),
+            CKM_SHA512 => Ok(MechanismType::SHA512),
             other => {
                 error!("Mechanism type {} is not supported.", other);
                 Err(Error::NotSupported)
@@ -52,8 +72,17 @@ impl TryFrom<CK_MECHANISM_TYPE> for MechanismType {
 }
 
 pub enum Mechanism {
+    // RSA
     RsaPkcsKeyPairGen,
     RsaPkcs,
+    RsaPkcsPss(rsa::PkcsPssParams),
+    RsaPkcsOaep(rsa::PkcsOaepParams),
+
+    // SHA-n
+    Sha1,
+    Sha256,
+    Sha384,
+    Sha512,
 }
 
 impl Mechanism {
@@ -61,6 +90,13 @@ impl Mechanism {
         match self {
             Mechanism::RsaPkcsKeyPairGen => MechanismType::RSA_PKCS_KEY_PAIR_GEN,
             Mechanism::RsaPkcs => MechanismType::RSA_PKCS,
+            Mechanism::RsaPkcsPss(_) => MechanismType::RSA_PKCS_PSS,
+            Mechanism::RsaPkcsOaep(_) => MechanismType::RSA_PKCS_OAEP,
+
+            Mechanism::Sha1 => MechanismType::SHA1,
+            Mechanism::Sha256 => MechanismType::SHA256,
+            Mechanism::Sha384 => MechanismType::SHA384,
+            Mechanism::Sha512 => MechanismType::SHA512,
         }
     }
 }
@@ -69,7 +105,27 @@ impl From<&Mechanism> for CK_MECHANISM {
     fn from(mech: &Mechanism) -> Self {
         let mechanism = mech.mechanism_type().into();
         match mech {
-            Mechanism::RsaPkcsKeyPairGen | Mechanism::RsaPkcs => CK_MECHANISM {
+            Mechanism::RsaPkcsPss(params) => CK_MECHANISM {
+                mechanism,
+                pParameter: params as *const _ as *mut c_void,
+                ulParameterLen: std::mem::size_of::<CK_RSA_PKCS_PSS_PARAMS>()
+                    .try_into()
+                    .expect("usize can not fit in CK_ULONG"),
+            },
+            Mechanism::RsaPkcsOaep(params) => CK_MECHANISM {
+                mechanism,
+                pParameter: params as *const _ as *mut c_void,
+                ulParameterLen: std::mem::size_of::<CK_RSA_PKCS_OAEP_PARAMS>()
+                    .try_into()
+                    .expect("usize can not fit in CK_ULONG"),
+            },
+            // Mechanisms without parameters
+            Mechanism::RsaPkcsKeyPairGen
+            | Mechanism::RsaPkcs
+            | Mechanism::Sha1
+            | Mechanism::Sha256
+            | Mechanism::Sha384
+            | Mechanism::Sha512 => CK_MECHANISM {
                 mechanism,
                 pParameter: null_mut(),
                 ulParameterLen: 0,
--- pkcs11/src/new/types/mechanism/rsa.rs
+++ pkcs11/src/new/types/mechanism/rsa.rs
@@ -1,1 +1,135 @@
+use crate::new::types::mechanism::{Mechanism, MechanismType};
+use crate::new::types::Ulong;
+use crate::new::{Error, Result};
+use log::error;
+use pkcs11_sys::*;
+use std::convert::TryFrom;
+use std::ffi::c_void;
+use std::ops::Deref;
 
+#[derive(Debug, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct PkcsMgfType {
+    val: CK_RSA_PKCS_MGF_TYPE,
+}
+
+impl PkcsMgfType {
+    pub const MGF1_SHA1: PkcsMgfType = PkcsMgfType { val: CKG_MGF1_SHA1 };
+    pub const MGF1_SHA224: PkcsMgfType = PkcsMgfType {
+        val: CKG_MGF1_SHA224,
+    };
+    pub const MGF1_SHA256: PkcsMgfType = PkcsMgfType {
+        val: CKG_MGF1_SHA256,
+    };
+    pub const MGF1_SHA384: PkcsMgfType = PkcsMgfType {
+        val: CKG_MGF1_SHA384,
+    };
+    pub const MGF1_SHA512: PkcsMgfType = PkcsMgfType {
+        val: CKG_MGF1_SHA512,
+    };
+}
+
+impl Deref for PkcsMgfType {
+    type Target = CK_RSA_PKCS_MGF_TYPE;
+
+    fn deref(&self) -> &Self::Target {
+        &self.val
+    }
+}
+
+impl From<PkcsMgfType> for CK_RSA_PKCS_MGF_TYPE {
+    fn from(mgf_type: PkcsMgfType) -> Self {
+        *mgf_type
+    }
+}
+
+impl TryFrom<CK_RSA_PKCS_MGF_TYPE> for PkcsMgfType {
+    type Error = Error;
+
+    fn try_from(mgf_type: CK_RSA_PKCS_MGF_TYPE) -> Result<Self> {
+        match mgf_type {
+            CKG_MGF1_SHA1 => Ok(PkcsMgfType::MGF1_SHA1),
+            CKG_MGF1_SHA224 => Ok(PkcsMgfType::MGF1_SHA224),
+            CKG_MGF1_SHA256 => Ok(PkcsMgfType::MGF1_SHA256),
+            CKG_MGF1_SHA384 => Ok(PkcsMgfType::MGF1_SHA384),
+            CKG_MGF1_SHA512 => Ok(PkcsMgfType::MGF1_SHA512),
+            other => {
+                error!(
+                    "Mask Generation Function type {} is not one of the valid values.",
+                    other
+                );
+                Err(Error::InvalidValue)
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct PkcsOaepSourceType {
+    val: CK_RSA_PKCS_OAEP_SOURCE_TYPE,
+}
+
+impl PkcsOaepSourceType {
+    pub const DATA_SPECIFIED: PkcsOaepSourceType = PkcsOaepSourceType {
+        val: CKZ_DATA_SPECIFIED,
+    };
+}
+
+impl Deref for PkcsOaepSourceType {
+    type Target = CK_RSA_PKCS_OAEP_SOURCE_TYPE;
+
+    fn deref(&self) -> &Self::Target {
+        &self.val
+    }
+}
+
+impl From<PkcsOaepSourceType> for CK_RSA_PKCS_OAEP_SOURCE_TYPE {
+    fn from(pkcs_oaep_source_type: PkcsOaepSourceType) -> Self {
+        *pkcs_oaep_source_type
+    }
+}
+
+impl TryFrom<CK_RSA_PKCS_OAEP_SOURCE_TYPE> for PkcsOaepSourceType {
+    type Error = Error;
+
+    fn try_from(pkcs_oaep_source_type: CK_RSA_PKCS_OAEP_SOURCE_TYPE) -> Result<Self> {
+        match pkcs_oaep_source_type {
+            CKZ_DATA_SPECIFIED => Ok(PkcsOaepSourceType::DATA_SPECIFIED),
+            other => {
+                error!("OAEP source type {} is not one of the valid values.", other);
+                Err(Error::InvalidValue)
+            }
+        }
+    }
+}
+
+/// Abstraction over CK_RSA_PKCS_PSS_PARAMS, share the same memory representation.
+#[repr(C)]
+pub struct PkcsPssParams {
+    pub hash_alg: MechanismType,
+    pub mgf: PkcsMgfType,
+    pub s_len: Ulong,
+}
+
+impl From<PkcsPssParams> for Mechanism {
+    fn from(pkcs_pss_params: PkcsPssParams) -> Self {
+        Mechanism::RsaPkcsPss(pkcs_pss_params)
+    }
+}
+
+/// Abstraction over CK_RSA_PKCS_OAEP_PARAMS, share the same memory representation.
+#[repr(C)]
+pub struct PkcsOaepParams {
+    pub hash_alg: MechanismType,
+    pub mgf: PkcsMgfType,
+    pub source: PkcsOaepSourceType,
+    pub source_data: c_void,
+    pub source_data_len: Ulong,
+}
+
+impl From<PkcsOaepParams> for Mechanism {
+    fn from(pkcs_oaep_params: PkcsOaepParams) -> Self {
+        Mechanism::RsaPkcsOaep(pkcs_oaep_params)
+    }
+}
